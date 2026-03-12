15be6cbc1d028aea41f20504bcd4f6081ad776a8
jiface <trapizoids543@gmail.com>
2026-03-12 17:51:57 +1100

    123123123123123

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ["src/app/wasm/**"],
+  },
 ];
 
 export default eslintConfig;
